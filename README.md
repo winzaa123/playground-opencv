e9d4c7ee39604c9effd462e2a0e318dfa95717ed
Win <winzaza@msn.com>
2019-06-21 08:21:55 +0700

    Update README.

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ aptitude search boost
 python -m pip install -U matplotlib
 pip3 install pillow
 pip install scipy
+
+pip install opencv-python
+apt-get install libsm6 libxrender1 libfontconfig1
+
+apt-get install libxext6
+
+apt-get install python-pip python-dev python-virtualenv
+virtualenv --system-site-packages ~/tensorflow
+source ~/tensorflow/bin/activate
+
+# object detection
+
+https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md
+<!-- apt-get install protobuf-compiler python-pil python-lxml
+pip install jupyter
+pip install matplotlib
+wget https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-linux-x86_64.zip -->
+
+cp -r pycocotools ~/tensorflow/models/research/
---
+++ mnt/tutorials/images.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images.shape      
+
+len(train_labels)
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
